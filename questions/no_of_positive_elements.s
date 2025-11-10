e165c429bfae2628b66a8b0b1db2f9924d37e6a7
kathan-205 <111491624+kathan-205@users.noreply.github.com>
2025-11-10 13:26:01 -0700

    Added a question which calculates no of positive elements from an integer array

---
+++ questions/no_of_positive_elements.s
@@ -0,0 +1,33 @@
+# This is the code to find the no of positive elements in an integer array.
+.data
+array:
+	.word  0, 4, 7, -8
+.text
+.global main
+
+main:
+	la s0,array
+	li t3,4 #length of the array
+	li t0,0
+	li t1,0
+
+loop_start:
+	beq t1,t3, loop_end
+	slli t2,t1,2
+	add t4, s0,t2
+	lw t5, 0(t4)
+	blez t5,next
+	addi t0,t0,1
+
+next:
+	addi t1,t1,1
+	jal zero, loop_start
+	
+loop_end:
+	mv a0,t0
+	li a7,1
+	ecall
+	
+	li a7,10
+	ecall
+	
